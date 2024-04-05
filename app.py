0bcb2bf97034bb74605012ea765528611b1ce7c5
Thu Ya Kyaw <contact.thuya@gmail.com>
2024-04-05 18:10:21 +0000

    upgraded to new UI

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ from flask import (
     redirect
 )
 
+import random
+import string
+
 import vertexai
 import vertexai.preview.generative_models as generative_models
 from vertexai.preview.generative_models import GenerativeModel, Image
+
+from google.cloud import storage
+
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
@@ -62,9 +68,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/generate', methods=['GET'])
-def generate():
-    return render_template('generate.html')
+def write_response(response, folder_name, file_name):
+    file_path = os.path.join(folder_name, file_name)
+    with open(file_path, "w") as file:
+        file.write(response)
+
+
+def create_public_html_file(html_content):
+    """Creates a new HTML file in a Google Cloud Storage bucket, makes it public, and returns the public URL."""
+
+    def generate_random_filename(length=10):
+        """Generates a random filename with letters and digits."""
+        characters = string.ascii_letters + string.digits
+        filename = ''.join(random.choice(characters) for i in range(length))
+        return filename + ".html"
+
+    bucket_name = "sketch-to-web"
+    filename = generate_random_filename()
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+
+    blob.upload_from_string(html_content, content_type='text/html')
+    blob.make_public()
+    return blob.public_url
 
 
 @app.route('/response', methods=['GET', 'POST'])
@@ -73,12 +101,19 @@ def response():
         uploaded_file = request.files['image-upload']
         wireframe_image = Image.from_bytes(uploaded_file.read())
         response = prompt(wireframe_image)
+
         if response:
-            return response
+            public_url = create_public_html_file(response)
+
+            return render_template(
+                'response.html', 
+                response=response, 
+                public_url=public_url, 
+            )
     else:
-        return redirect('/generate')
+        return redirect('/')
 
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
-    app.run(debug=False, port=server_port, host='0.0.0.0')
+    app.run(debug=True, port=server_port, host='0.0.0.0')
