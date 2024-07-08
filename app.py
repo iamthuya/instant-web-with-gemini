ffa9a284937a79c77ead8c64d165d644c62a46fc
Thu Ya Kyaw <contact.thuya@gmail.com>
2024-07-08 18:35:39 +0000

    (WIP) bug: fixed unintentional openning of file selector

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def response():
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
-    app.run(debug=False, port=server_port, host='0.0.0.0')
+    app.run(debug=True, port=server_port, host='0.0.0.0')
