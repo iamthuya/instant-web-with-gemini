140961cdc3e18ac7db4b393db7eea532fa7fe332
Thu Ya Kyaw <contact.thuya@gmail.com>
2024-07-16 03:52:12 +0000

    (Fix): Fixed the image not sending over to backend properly

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import os
 import random
 import string
+import base64
 
 import vertexai
 import vertexai.preview.generative_models as generative_models
 from flask import Flask, redirect, render_template, request
 from google.cloud import storage
-from tenacity import retry, stop_after_attempt
-from vertexai.preview.generative_models import GenerativeModel, Image
+from tenacity import retry, wait_random, stop_after_attempt
+from vertexai.preview.generative_models import GenerativeModel, Part
+
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 
-@retry(stop=stop_after_attempt(5))
+
+@retry(wait=wait_random(min=2, max=4), stop=stop_after_attempt(30))
 def generate(wireframe, model, prompt):
     model = GenerativeModel(model)
+    suffix = "Just provide the code without the explaination."
+
     contents = [
         wireframe,
-        prompt
+        prompt,
+        suffix
     ]
 
     responses = model.generate_content(
@@ -47,36 +53,58 @@ def index():
     return render_template('index.html')
 
 
-def create_public_html_file(html_content):
+def generate_random_characters(length=10):
+    """Generates a random filename with letters and digits."""
+    characters = string.ascii_letters + string.digits
+    random_characters = ''.join(random.choice(characters) for i in range(length))
+    return random_characters
+
+
+def create_public_html_file_on_gcs(random_characters, html_content):
     """Creates a new HTML file in a Google Cloud Storage bucket, makes it public, and returns the public URL."""
-
-    def generate_random_filename(length=10):
-        """Generates a random filename with letters and digits."""
-        characters = string.ascii_letters + string.digits
-        filename = ''.join(random.choice(characters) for i in range(length))
-        return filename + ".html"
-
-    bucket_name = "sketch-to-web"
-    filename = generate_random_filename()
+    filename = f"{GCS_FOLDER_NAME}/web-pages/{random_characters}.html"
 
     storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(filename)
+    bucket = storage_client.bucket(GCS_BUCKET_NAME)
 
+    blob = bucket.blob(filename)
     blob.upload_from_string(html_content, content_type='text/html')
     blob.make_public()
+
     return blob.public_url
 
+
+def save_wireframe_to_gcs(random_characters, base64_image_string):
+    """Store the wireframe in a Google Cloud Storage bucket and returns the g URL."""
+    filename = f"{GCS_FOLDER_NAME}/wireframes/{random_characters}.jpeg"
+    image_data = base64.b64decode(base64_image_string.replace("data:image/jpeg;base64,", ""))
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(GCS_BUCKET_NAME)
+
+    content_type = 'image/jpeg'
+    blob = bucket.blob(filename)
+    blob.upload_from_string(image_data, content_type=content_type)
+
+    wireframe = Part.from_uri(
+        mime_type=content_type,
+        uri=f"gs://{GCS_BUCKET_NAME}/{filename}"
+    )
+
+    return wireframe
 
 @app.route('/response', methods=['GET', 'POST'])
 def response():
     if request.method == 'POST':
-        wireframe = request.form['capturedImage']
+        base64_image_string = request.form['capturedImage']
         model = request.form['model']
         prompt = request.form['prompt']
+        random_characters = generate_random_characters()
 
         try:
+            wireframe = save_wireframe_to_gcs(random_characters, base64_image_string)
             response = generate(wireframe, model, prompt)
+            response = response.replace("```html", "").replace("```", "").strip()
 
         except ValueError as e:
             error_mesages = [
@@ -94,7 +122,7 @@ def response():
             response = f"<h1> {random_message} </h1>"
 
         if response:
-            public_url = create_public_html_file(response)
+            public_url = create_public_html_file_on_gcs(random_characters, response)
 
             return render_template(
                 'response.html',
@@ -106,9 +134,11 @@ def response():
 
 
 if __name__ == '__main__':
-    server_port = os.environ.get('PORT', '8080')
-    location = os.environ.get('LOCATION', "us-central1")
-    project_id = os.environ.get('PROJECT_ID', "thuya-next-demos")
-    vertexai.init(project=project_id, location=location)
+    PORT = os.environ.get('PORT', '8080')
+    LOCATION = os.environ.get('LOCATION', "us-central1")
+    PROJECT_ID = os.environ.get('PROJECT_ID', "thuya-next-demos")
+    GCS_BUCKET_NAME = os.environ.get('GCS_BUCKET_NAME', "instant-web-gemini")
+    GCS_FOLDER_NAME = os.environ.get('GCS_FOLDER_NAME', "next-tokyo-2024")
 
-    app.run(debug=True, port=server_port, host='0.0.0.0')
+    vertexai.init(project=PROJECT_ID, location=LOCATION)
+    app.run(debug=False, port=PORT, host='0.0.0.0')
